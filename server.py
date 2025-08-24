420092d7a09d088287bf8f8dc0abd48ee7c95e94
menezesmatheus5123-a11y <menezesmatheus5123@gmail.com>
2025-08-24 14:10:38 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -17,5 +16,4 @@ def get_buffs():
     return jsonify({"buffs": []})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
